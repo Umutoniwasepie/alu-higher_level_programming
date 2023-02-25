63c943e3d018c2e8c5f1f4b2ce70791e79fa8c04
Umutoniwasepie <p.umutoniwa@alustudent.com>
2023-02-25 08:40:55 -0800

    task 0

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Python program that:
+# demonstrates how to import a function def add(a, b)
+# Import the add function
+from task import add
+
+a = 1
+b = 2
+
+# This code should not run when this file is imported
+if __name__ == "__main__":
+    print("{:d} + {:d} = {:d}". format(a, b, add(a, b)))
